5f9729b18e6282bd3164281336bbb42a99e1fa64
aaron-ekal <ekalaaronkalale@gmail.com>
2022-03-24 01:33:02 +0300

    modified .js & .css files

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let typed = new Typed(".auto-type", {
+    strings: [
+      "Hi Adventurer",
+      "Brings Africa Close",
+      "makes it happen!",
+      "Ensures Your Security",
+      "Your Travelling Companion",
+      "Ensures Your Money's Worth",
+      "is Economical",
+      "Promotes Eco-Tourism",
+      "Cares for you!",
+    ],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true,
+  });
+  
