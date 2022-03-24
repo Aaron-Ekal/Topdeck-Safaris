848471e36b02fabcbb502ccd49f6f3c4bd2108ae
aaron-ekal <ekalaaronkalale@gmail.com>
2022-03-25 01:05:21 +0300

    modified index/about.html files

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-
 // alert("hi");
 let modal = $(".modalBox");
 
@@ -119,22 +118,3 @@ test = () => {
 //   } else console.log("valid input");
 //   bookingDetails["noOfAdults"] = numberAdults;
 // };
-
-let typed = new Typed(".auto-type", {
-  strings: [
-    "Hi Adventurer",
-    "Brings Adventure To You",
-    "makes it happen!",
-    "Ensures Your Security",
-    "Your Travelling Companion",
-    "Ensures Your Money's Worth",
-    "is Economical",
-    "Promotes Eco-Tourism",
-    "Cares for you!",
-    "Book Now!",
-  ],
-  typeSpeed: 150,
-  backSpeed: 150,
-  loop: true,
-});
-
